5b9782ee4cead0aa4a614737cea5dfcf2d871533
josuehoenicka <josuee.vzla@gmail.com>
2023-09-27 21:18:03 -0300

    new: basic calculator project

---
+++ 3 - C#/2 - Basic Calculator/BasicCalculator/BasicCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿int number1;
+int number2;
+int result;
+string answer;
+
+Console.WriteLine("Welcome to my basic calculator !");
+Console.WriteLine("Please enter your first number: ");
+
+number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter your second number: ");
+
+number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("What type of operation would you like to do ?");
+Console.WriteLine("Please enter + for addition, - for subtraction, * for multiplication or / for division");
+
+answer = Console.ReadLine();
+
+if(answer == "+")
+{
+    result = number1 + number2;
+} else if(answer == "-")
+{
+    result = number1 - number2;
+} else if(answer == "*")
+{
+    result = number1 * number2;
+} else if(answer == "/")
+{
+    result = number1 / number2;
+} else
+{
+    Console.WriteLine("You introduce an invalid type of operation :(");
+}
+
+Console.WriteLine("The result is " + result);
+
+Console.ReadKey();
