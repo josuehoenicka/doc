24fa32dc22293809a3a8ace0b2c20b5718e1eab6
josuehoenicka <josuee.vzla@gmail.com>
2023-11-01 17:49:00 -0300

    fix: delete new name

---
+++ .NET/Projects/4 - Shape Area Calculator/ShapeAreaCalculator/ShapeAreaCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿string answer;
+float result;
+
+Console.WriteLine("What shape would you like to find the area of? Please enter 'r' for rectangle or 'c' for circle");
+Console.WriteLine("");
+
+answer = Console.ReadLine();
+
+if(answer == "r")
+{
+    Console.WriteLine("");
+    Console.WriteLine("Please enter the height of the rectangle");
+    float height = float.Parse(Console.ReadLine());
+    Console.WriteLine("");
+    Console.WriteLine("Please enter the width of the rectangle");
+    float width = float.Parse(Console.ReadLine());
+    result = height * width;
+    Console.WriteLine("");
+    Console.WriteLine("The result is " + result);
+}
+if(answer == "c")
+{
+    Console.WriteLine("");
+    Console.WriteLine("Please enter the radius of the circle");
+    float radius = float.Parse(Console.ReadLine());
+    result = (float)Math.PI * (radius * radius);
+    Console.WriteLine("");
+    Console.WriteLine("The result is " + result);
+}
+if(answer != "r" && answer != "c")
+{
+    Console.WriteLine("ERROR! Invalid selection. Press any key to exit");
+}
+Console.ReadKey();
